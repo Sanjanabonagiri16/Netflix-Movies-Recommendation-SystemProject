7e3bb89f74fee0b86f85bb0c91488b98d0db3c09
Sanjanabonagiri16 <bonagirisanjana1116@gmail.com>
2024-07-16 18:31:37 +0530

    Add files via upload

---
+++ netflix.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction import text
+from sklearn.metrics.pairwise import cosine_similarity
+import nltk
+import re
+data = pd.read_csv("netflix.csv")
+print(data.head())
+print(data.isnull().sum())
+print(data.head())
+data = data.dropna()
+nltk.download('stopwords')
+stemmer = nltk.SnowballStemmer("english")
+from nltk.corpus import stopwords
+import string
+stopword=set(stopwords.words('english'))
+
+def clean(text):
+    text = str(text).lower()
+    text = re.sub(r'\[.*?\]', '', text)
+    text = re.sub(r'https?://\S+|www\.\S+', '', text)
+    text = re.sub(r'<.*?>+', '', text)
+    text = re.sub(r'[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub(r'\n', '', text)
+    text = re.sub(r'\w*\d\w*', '', text)
+    text = [word for word in text.split(' ') if word not in stopword]
+    text=" ".join(text)
+    text = [stemmer.stem(word) for word in text.split(' ')]
+    text=" ".join(text)
+    return text
+indices = pd.Series(data.index, 
+                    index=data['Title']).drop_duplicates()
+def netFlix_recommendation(title, similarity = similarity):
+    index = indices[title]
+    similarity_scores = list(enumerate(similarity[index]))
+    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+    similarity_scores = similarity_scores[0:10]
+    movieindices = [i[0] for i in similarity_scores]
+    return data['Title'].iloc[movieindices]
+
+print(netFlix_recommendation("girlfriend"))
